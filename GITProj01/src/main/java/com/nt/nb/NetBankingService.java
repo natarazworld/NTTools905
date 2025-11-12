6debdfb7c449b13a6cd0f63552e57ed8eee363bb
nataraz <natarazjavaarena@gmail.com>
2025-11-12 10:26:46 +0530

    tag modified

--- GITProj01/src/main/java/com/nt/nb/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/nb/NetBankingService.java
@@ -3,7 +3,7 @@ package com.nt.nb;
 public class NetBankingService {
 	
 	public String transferMoney(long srcAcno,long destAcno, double amt,String gateway) {
-		return  amt+" amount is transfered from "+srcAcno+" to"+ destAcno + "using gateway "+gateway;
+		return  amt+" amount is transfered from "+srcAcno+" to"+ destAcno + "using gateway... "+gateway;
 	}
 	
 	
