4a553308eacc9bf5eab400b68b03d9a1c5adf38f
nataraz <natarazjavaarena@gmail.com>
2025-11-12 10:11:18 +0530

    TL created the B2 Branch

---
+++ GITProj01/src/main/java/com/nt/nb/NetBankingService.java
@@ -0,0 +1,9 @@
+package com.nt.nb;
+
+public class NetBankingService {
+	
+	public String transferMoney(long srcAcno,long destAcno, double amt) {
+		return  amt+" amount is transfered from "+srcAcno+" to"+ destAcno;
+	}
+
+}
