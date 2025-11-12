85871aabaad9ae5c0d9f61076a0372e347100528
nataraz <natarazjavaarena@gmail.com>
2025-11-12 09:53:52 +0530

    UPI Payment is completed by Dev1

---
+++ GITProj01/src/main/java/com/nt/upi/UPIPayment.java
@@ -0,0 +1,9 @@
+package com.nt.upi;
+
+public class UPIPayment {
+	
+	public  String  doUPIPayment(long mobileno, float amount) {
+		return amount+" amount is transffered to  mobile number ::"+mobileno +" Using BharatPe";
+	}
+
+}
