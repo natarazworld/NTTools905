c24b7f6b9f480d8a49a7d4727bd18b1c132cf96a
nataraz <natarazjavaarena@gmail.com>
2025-11-13 09:28:02 +0530

    dev1 chnage1

--- GITProj01/src/main/java/com/nt/nb/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/nb/NetBankingService.java
@@ -2,7 +2,9 @@ package com.nt.nb;
 
 public class NetBankingService {
 	
+	 // for netbanking
 	public String transferMoney(long srcAcno,long destAcno, double amt,String gateway) {
+		System.out.println("NetBankingService.transferMoney()");
 		return  amt+" amount is transfered from "+srcAcno+" to"+ destAcno + "using gateway "+gateway;
 	}
 	
