b17dec72181029a5c0e87dfea57e1b6633642aa6
nataraz <natarazjavaarena@gmail.com>
2025-11-13 09:30:31 +0530

    dev1  merging

--- GITProj01/src/main/java/com/nt/nb/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/nb/NetBankingService.java
@@ -4,12 +4,8 @@ public class NetBankingService {
 	
 	 // for netbanking
 	public String transferMoney(long srcAcno,long destAcno, double amt,String gateway) {
-<<<<<<< HEAD
-		return  amt+" amount is transfered from "+srcAcno+" to"+ destAcno + "using gateway... "+gateway;
-=======
 		System.out.println("NetBankingService.transferMoney()");
 		return  amt+" amount is transfered from "+srcAcno+" to"+ destAcno + "using gateway "+gateway;
->>>>>>> refs/remotes/origin/B2-NetBanking
 	}
 	
 	
