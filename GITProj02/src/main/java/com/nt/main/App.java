b46d126d1b0a3605413a42134ef92be9b038905c
nataraz <natarazjavaarena@gmail.com>
2025-11-13 10:07:41 +0530

    change2

--- GITProj02/src/main/java/com/nt/main/App.java
+++ GITProj02/src/main/java/com/nt/main/App.java
@@ -6,5 +6,6 @@ package com.nt.main;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!  123");
+        System.out.println("welcome");
     }
 }
